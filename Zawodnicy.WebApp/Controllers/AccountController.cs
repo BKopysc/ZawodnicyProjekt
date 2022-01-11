156bc8bb45e7baa5a5a2b3416d520c82c3244cdd
Bartlomiej Kopysc <bkopysc@wp.pl>
2022-01-11 23:07:50 +0100

    Dodanie rejestracji i logowania i kilku fixow

--- Zawodnicy.WebApp/Controllers/AccountController.cs
+++ Zawodnicy.WebApp/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Zawodnicy.WebApp.Models;
 
 namespace Zawodnicy.WebApp.Controllers
 {
@@ -25,9 +26,76 @@ namespace Zawodnicy.WebApp.Controllers
         {
             return View();
         }
-        public IActionResult Index()
+
+        public IActionResult Register()
         {
-            return View();
+            return View(new LoginVM());
+        }
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM loginVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(loginVM);
+            }
+
+            var user = await _userManager.FindByNameAsync(loginVM.UserName);
+
+            if(user != null)
+            {
+                //logowanie
+                var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+            ModelState.AddModelError("", "Niepoprawna nazwa użytkownika lub hasło...");
+
+            return View(loginVM);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(LoginVM loginVM)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = new IdentityUser()
+                {
+                    UserName = loginVM.UserName
+                };
+
+                Console.WriteLine("Register");
+
+                var result = await _userManager.CreateAsync(user, loginVM.Password);
+
+                if (result.Succeeded)
+                {
+                    TempData["RegisterMessage"] = "Success";
+                    return RedirectToAction("Index", "Home");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+            return View(loginVM);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
